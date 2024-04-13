3b8af48677f1045f9eacdce9a25291be20376142
Vladyslav Levkovich <levkovich.vlad.2004@gmail.com>
2024-04-13 18:41:19 +0300

    aquarium

--- src/fish/views.py
+++ src/fish/views.py
@@ -6,11 +6,12 @@ from .models import *
 
 
 class FeedAquarium(generics.GenericAPIView):
+    """годуємо рибок в акваріумі"""
     permission_classes = [permissions.IsAuthenticated]
 
     def put(self, request):
         try:
-            aquarium = Aquarium.objects.get(user=request.user)
+            aquarium = Aquarium.objects.filter(user=request.user).first()
         except Aquarium.DeosNotExist:
             return Response({'message': 'Aquarium not found'}, status=status.HTTP_404_NOT_FOUND)
 
@@ -19,17 +20,50 @@ class FeedAquarium(generics.GenericAPIView):
         return Response({'message': 'OK'}, status=status.HTTP_200_OK)
 
 
+class AddFish(generics.GenericAPIView):
+    serializer_class = AddFishSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        print(request.user)
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class AddAlgae(generics.GenericAPIView):
+    serializer_class = AddAlgaeSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class AddShrimp(generics.GenericAPIView):
+    serializer_class = AddShrimpSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
 class AquariumReadCreate(generics.GenericAPIView):
     serializer_class = ResidentSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
-        aquarium = Aquarium.objects.get(user=request.user)
+        aquarium = Aquarium.objects.filter(user=request.user).first()
         serializer = self.serializer_class(aquarium)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
         print('USER', request.user)
-        serializer = self.serializer_class(data=request.data, context={'request': request.user})
+        serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
