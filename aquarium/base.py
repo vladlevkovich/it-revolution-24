568c8d7f192b73650c358d9b6382cafb3f992b9e
Vladyslav Levkovich <levkovich.vlad.2004@gmail.com>
2024-04-14 11:47:43 +0300

    Update base.py

--- aquarium/base.py
+++ aquarium/base.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^b9qbrm(_!zhebygd7e$-d!$mm+ggoc+lrjdntf&92ti9a-d(s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Application definition
 
